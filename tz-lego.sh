0c06869ceac6f5a5ead14d62a56937158af51de7
janniktaulan <janniktn1999@hotmail.com>
2025-10-21 09:46:57 +0200

    bugfixing

--- tz-lego.sh
+++ tz-lego.sh
@@ -227,13 +227,12 @@ function new_cert() {
             sudo systemctl restart $server
             if [[ $renewal = yes ]]; then
                 echo "Creating cronjob for automatic renewal at: /etc/lego/scripts/renewal.sh"
-                sudo sed -E "/(^|:)sudo systemctl restart ${server}(:|$)/d" /etc/lego/scripts/renewal.sh
+                sudo sed -E "/sudo systemctl restart ${server}/d" /etc/lego/scripts/renewal.sh
                 echo "sudo lego $registration $val_manual $eab $domain_renew_var" >> /etc/lego/scripts/renewal.sh
+                echo "sudo systemctl restart $server" >> /etc/lego/scripts/renewal.sh
                 if [[ $path = true ]]; then
                     echo "sudo cp /var/snap/lego/common/.lego/certificates/* "$custom_path"" >> /etc/lego/scripts/renewal.sh
                 fi
-                echo "sudo systemctl restart $server" >> /etc/lego/scripts/renewal.sh
-                echo "" >> /etc/lego/scripts/renewal.sh
             fi
             if [[ $path = true ]]; then
                 copy_certs

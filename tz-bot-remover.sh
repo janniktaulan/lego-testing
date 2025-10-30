4eb6e848311abc8db7e9f1ad3f405c3bafcae06f
janniktaulan <janniktn1999@hotmail.com>
2025-10-30 16:15:02 +0100

    test

--- tz-bot-remover.sh
+++ tz-bot-remover.sh
@@ -1,15 +1,15 @@
 # UNDER CONSTRUCTION
 echo "Welcome to the TZ-Bot and Lego uninstaller."
 echo "This script will uninstall TZ-Bot and Lego from your system."
-echo "It will also remove all certificates from /etc/lego/certs/ and all scripts from /etc/lego/scripts/."
+echo "It will also remove all certificates from /etc/tz-bot/certs/ and all scripts from /etc/tz-bot/scripts/."
 read -n 1 -p "Are you sure you want to proceed? (y/n): " confirm_uninstall
 echo
 if [[ "$confirm_uninstall" == "y" ]]; then
     echo "Uninstalling TZ-Bot and Lego..."
-    sudo rm -rf /etc/lego/
-    sudo rm -rf /usr/local/bin/lego
+    sudo rm -rf /etc/tz-bot/
     sudo rm -rf /usr/local/bin/tz-bot
-    sudo crontab -l | grep -v '/etc/lego/scripts/renewal.sh' | sudo crontab -
+    sudo rm -rf /usr/local/bin/lego
+    sudo crontab -l | grep -v '/etc/tz-bot/scripts/renewal.sh' | sudo crontab -
     if command -v lego >/dev/null 2>&1; then
         echo "Uninstallation failed. Please remove manually."
         exit
